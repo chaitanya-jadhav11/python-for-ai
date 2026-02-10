7465534cf68e07bc957cd1be83bdd376c6102097
chaitanya-jadhav11 <chaitanyabj@gmail.com>
2026-02-10 21:41:39 +0530

    Main and name

---
+++ 00-Modules_and_Packages/myprogram.py
@@ -0,0 +1,13 @@
+# use the function from the module
+from mymodule import my_func
+my_func()
+
+# Now we will import the function from the package
+from MyMainPackage import  some_main_script
+some_main_script.report_main()
+
+# Now we will import the function from the subpackage
+from MyMainPackage.SubPackage import mysubscript
+mysubscript.sub_report()
+
+
