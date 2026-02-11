47bcd4542c705de35fcc68e8fce2ed867db2d7f3
chaitanya-jadhav11 <chaitanyabj@gmail.com>
2026-02-11 12:47:48 +0530

    python pylint lib example

---
+++ 07-Errors and Exception Handling/simple.py
@@ -0,0 +1,18 @@
+"""
+This is simple script
+"""
+
+def myfunc():
+    """This is my function"""
+
+    first =1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
+
+#  pylint simple.py -r y
+
+# Your code has been rated at 8.33/10 (previous run: 8.33/10, +0.00)
+
